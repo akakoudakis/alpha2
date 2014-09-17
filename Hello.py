634fbd3709d4db7cc88dc92a91b581fc3b080a96
Anthony <akakoudakis@yahoo.com>
2014-09-17 22:57:22 +0100

    file change

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
-print "hello world"
+import os
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello World!'
+    port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0ort=int(port))
